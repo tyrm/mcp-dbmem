c335744cca74c58f967a1cf0be6a24d4aae07d88
Tyr Mactire <tyr@pettingzoo.co>
2025-05-20 12:37:21 -0700

    chaos

--- cmd/mcp_dbmem/action/server/server.go
+++ cmd/mcp_dbmem/action/server/server.go
@@ -7,13 +7,11 @@ import (
 	"os/signal"
 	"syscall"
 
-	mcp "github.com/metoro-io/mcp-golang"
-	"github.com/metoro-io/mcp-golang/transport/stdio"
 	"github.com/spf13/viper"
 	"github.com/tyrm/mcp-dbmem/cmd/mcp_dbmem/action"
-	"github.com/tyrm/mcp-dbmem/internal/adapter"
 	"github.com/tyrm/mcp-dbmem/internal/config"
 	"github.com/tyrm/mcp-dbmem/internal/db/bun"
+	"github.com/tyrm/mcp-dbmem/internal/http"
 	v1 "github.com/tyrm/mcp-dbmem/internal/logic/v1"
 	"github.com/uptrace/uptrace-go/uptrace"
 	"go.uber.org/zap"
@@ -22,6 +20,7 @@ import (
 // Server is the action to start the mcp server with a direct connection to the database.
 var Server action.Action = func(ctx context.Context, _ []string) error {
 	zap.L().Info(fmt.Sprintf("starting %s server", config.ApplicationName))
+	ctx, cancel := context.WithCancel(ctx)
 
 	// Setup tracing
 	if viper.GetString(config.Keys.UptraceDSN) != "" {
@@ -51,6 +50,7 @@ var Server action.Action = func(ctx context.Context, _ []string) error {
 	})
 	if err != nil {
 		zap.L().Error("Error creating bun client", zap.Error(err))
+		cancel()
 
 		return err
 	}
@@ -65,13 +65,14 @@ var Server action.Action = func(ctx context.Context, _ []string) error {
 		DB: dbClient,
 	})
 
-	direct := adapter.NewServerAdapter(logic)
-
-	// add tools
-	server := mcp.NewServer(stdio.NewStdioServerTransport())
-	if err := direct.Apply(server); err != nil {
-		zap.L().Error("Error applying direct adapter", zap.Error(err))
-
+	httpServer, err := http.NewServer(ctx, http.ServerConfig{
+		Logic:           logic,
+		ApplicationName: config.ApplicationName,
+		HttpBind:        "",
+	})
+	if err != nil {
+		zap.L().Error("can't start http server", zap.Error(err))
+		cancel()
 		return err
 	}
 
@@ -82,14 +83,14 @@ var Server action.Action = func(ctx context.Context, _ []string) error {
 	stopSigChan := make(chan os.Signal, 1)
 	signal.Notify(stopSigChan, syscall.SIGINT, syscall.SIGTERM)
 
-	// start mcp server
-	go func(s *mcp.Server, errChan chan error) {
-		zap.L().Info("starting mcp server")
-		err := s.Serve()
+	// start webserver
+	go func(s *http.Server, errChan chan error) {
+		zap.L().Info("starting http server")
+		err := s.Start()
 		if err != nil {
-			errChan <- fmt.Errorf("mcp server: %s", err.Error())
+			errChan <- fmt.Errorf("http server: %s", err.Error())
 		}
-	}(server, errChan)
+	}(httpServer, errChan)
 
 	// wait for event
 	select {
@@ -99,6 +100,7 @@ var Server action.Action = func(ctx context.Context, _ []string) error {
 		zap.L().Fatal("fatal error", zap.Error(err))
 	}
 
+	cancel()
 	zap.L().Info("done")
 	return nil
 }
